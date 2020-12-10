ce6a4dca818a2caa44f02353e22606058d4f3585
Oskar Almlöv <oskar@almlov.com>
2020-12-10 20:58:41 +0100

    Refactor day 5

--- 05/main.py
+++ 05/main.py
@@ -2,44 +2,50 @@
 
 # Part 1
 with open("data.txt") as input_file:
-    input = input_file.read()
+    boardingpasses = input_file.read().splitlines()
 
 
-def split_list(the_list):
-    half = len(the_list)//2
-    return the_list[:half], the_list[half:]
+def split_list(a_list):
+    half = len(a_list)//2
+    return a_list[:half], a_list[half:]
 
 
-def bin_search(seat_string, index, seat_range):
-    current_direction = seat_string[index]
+def bin_search(seat_string, seat_range):
+    direction = seat_string[0]
     lower_range, upper_range = split_list(seat_range)
 
-    if current_direction in ["F", "L"]:
+    if direction in ("F", "L"):
         new_range = lower_range
-    elif current_direction in ["B", "R"]:
+    elif direction in ("B", "R"):
         new_range = upper_range
-
     if len(new_range) == 1:
         return new_range[0]
 
-    index += 1
-    return bin_search(seat_string, index, new_range)
+    return bin_search(seat_string[1:], new_range)
+
+
+def split_boardingpass(boardingpass):
+    return boardingpass[:-3], boardingpass[-3:]
+
+
+def get_seat_id(row, column):
+    return row * 8 + column
 
 
 def main():
+    SEAT_ROWS = 128
+    SEAT_COLUMNS = 8
     seat_ids = []
-    boardingpasses = input.splitlines()
     for boardingpass in boardingpasses:
-        row_string = boardingpass[:-3]
-        column_string = boardingpass[-3:]
-        row = bin_search(row_string, 0, range(128))
-        column = bin_search(column_string, 0, range(8))
-        seat_ids.append((row * 8) + column)
+        row_str, column_str = split_boardingpass(boardingpass)
+        row = bin_search(row_str, range(SEAT_ROWS))
+        column = bin_search(column_str, range(SEAT_COLUMNS))
+        seat_ids.append(get_seat_id(row, column))
 
     # Highest seat ID
     print(max(seat_ids))
 
-    # Part 2
+# Part 2
     sorted_seat_ids = sorted(seat_ids)
     # Find seat ID missing from the list of seat IDs
     print([x for x in range(sorted_seat_ids[0],
